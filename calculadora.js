a71dcdae72e52f75cf2692030f4828451857f043
Bianca <bialisilva@hotmail.com>
2022-11-01 21:26:04 -0300

    Calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,81 @@
+//CALCULADORA NÍVEL 1
+
+function adicionar (x, y) {
+    return x + y;
+}
+
+console.log(5 + 5);
+
+function subtracao (primeiro, segundo){
+    return primeiro - segundo;
+}
+
+console.log(15 - 5);
+
+function multiplicacao (x1, x2){
+    return x1 * x2;
+}
+
+console.log(5 * 2);
+
+function multiplicacao (d1, d2){
+    return d1 / d2;
+}
+
+console.log(20 / 2);
+
+//CALCULADORA NÍVEL 2
+
+//1.string
+
+console.log("--Teste de Operações/Calculador--");
+
+//2.função somar e função subtrair
+
+console.log(5 + 5, 15 - 5);
+
+//3.função multiplicar
+
+console.log(5 * 2);
+
+//4.função dividir
+
+console.log(10 / 2);
+
+//5.função dividir argumento Zero
+
+console.log(0 / 10);
+
+//CALCULADORA NÍVEL 3
+
+//1. multiplicar por ele mesmo
+
+function quadradoDoNumero(Um) {
+    return Um * Um;
+}
+
+console.log (2 ** 4);
+
+//2.calcular a média de  números
+
+function mediaDeTresNumeros(n1, n2, n3) {
+    return ((n1, n2, n3)/ d2);
+}
+
+console.log((10 + 2 + 3)/ 3);
+
+//3.calcular o total da porcentagem
+
+function calculaPorcentagem(total, porcentagem) {
+    return ((total * porcentagem)/ valor);
+}
+
+console.log((300 * 15)/ 100);
+
+//4.porcentagem do primeiro em relação ao segundo
+
+function calculaPorcentagem(porcentagem, total) {
+    return ((porcentagem * total)/ porcentagem);
+}
+
+console.log((2 / 200)* 100);
